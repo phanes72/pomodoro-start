c113fab0a3379ea55b228fd4ab08954cb18d08f7
phanes72 <phanes72@gmail.com>
2024-03-17 09:14:21 -0600

    Added Canvas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +18,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+
+canvas = Canvas(width=200, height=224)
+tomato_img: PhotoImage = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
+
+
+
+
+
+
+window.mainloop()
